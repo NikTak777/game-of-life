9d0c6abcf61af912e3c4c84e43abb2bc30422771
NikTak777 <nikkondrakhin@gmail.com>
2026-06-11 23:01:36 +0300

    Добавлено: меню настроек, новый безграничный режим игры.

--- GameOfLife/GameOfLife.cpp
+++ GameOfLife/GameOfLife.cpp
@@ -4,6 +4,11 @@
 #include <cstring>
 #include <cstdlib>
 #include <ctime>
+#include <map>
+#include <conio.h>
+
+extern "C" void next_step(int count, bool* area, int hight, int width, bool* new_area);
+extern "C" void next_tor_step(int count, bool* area, int hight, int width, bool* new_area);
 
 using namespace std;
 
@@ -38,6 +43,9 @@ public:
         for (int i = 0; i < count; i++) {
             area[i] = (rand() % 100 < percentage);
         }
+
+        memset(history, 0, Width * sizeof(int));
+        current_percent = 0;
     }
 
     void InitializePattern(const std::string& name) {
@@ -104,7 +112,7 @@ public:
                     cout << "* ";
                 }
                 else if (current_level == 0) {
-                    cout << "_ ";
+                    cout << "* ";
                 }
                 else {
                     cout << "  ";
@@ -123,37 +131,113 @@ public:
     }
 };
 
-extern "C" void next_step(int count, bool* area, int hight, int width, bool* new_area);
+class Game {
+public:
+    GameArea Area;
+    int FPS;
+    unsigned int generation;
+    bool is_paused = false;
+    int current_mode;
+    map<int, string> mode_names;
+
+    Game(const int hight, const int width, int fps = 2) :
+    FPS(fps), Area(hight, width) {
+        current_mode = 2;
+        mode_names[1] = "Classic (Borders)";
+        mode_names[2] = "Torus (Seamless)";
+    }
+
+    void StartSimulation() {
+        Area.InitializeRandom(25);
+        generation = 1;
+        //Area.InitializePattern("glider");
+
+        GameLoop();
+    }
+
+    void GameLoop() {
+        while (true) {
+            if (_kbhit()) {
+                char key = _getch();
+
+                if (key == 27 || key == 'm' || key == 'M') {
+                    system("cls");
+                    cout << "==================================================" << endl;
+                    cout << "                MENU / PAUSE                      " << endl;
+                    cout << "==================================================" << endl;
+                    cout << " 1. Continue simulation" << endl;
+                    cout << " 2. Restart with new random field" << endl;
+                    cout << " 3. Toggle Game Mode (Current: " << mode_names[current_mode] << ")" << endl;
+                    cout << " 4. Exit game" << endl;
+                    cout << "==================================================" << endl;
+                    cout << "Choose option (1-4): ";
+
+                    char choice = _getch();
+
+                    if (choice == '2') {
+                        Area.InitializeRandom(25);
+                        generation = 1;
+                        system("cls");
+                        continue;
+                    }
+                    else if (choice == '3') {
+                        current_mode = (current_mode == 1) ? 2 : 1;
+                        system("cls");
+                        continue;
+                    }
+                    else if (choice == '4') {
+                        break;
+                    }
+                    system("cls");
+                }
+                else if (key == 'w' || key == 'W') {
+                    FPS++;
+                    if (FPS > 10) FPS = 10;
+                }
+                else if (key == 's' || key == 'S') {
+                    if (FPS > 1) {
+                        FPS--;
+                    }
+                }
+            }
+
+
+            Area.UpdateGraph();
+
+            cout << "============================================================================================================" << endl;
+            cout << "   Generation: " << generation << "   |   Alive: " << Area.current_percent << "%   |   FPS: " << FPS
+                << "   |   Mode: " << mode_names[current_mode] << "   |   Press Esc to Menu" << endl;
+            cout << "============================================================================================================" << endl;
+
+            Area.PrintArea();
+
+            if (current_mode == 1) {
+                next_step(Area.count, Area.area, Area.Hight, Area.Width, Area.new_area);
+            }
+            else {
+                next_tor_step(Area.count, Area.area, Area.Hight, Area.Width, Area.new_area);
+            }
+
+            if (!Area.UpdateFrame()) {
+                cout << "============================================================================================================" << endl;
+                cout << " Game over! Simulation stabilized at generation " << generation << "!" << endl;
+                cout << "============================================================================================================" << endl;
+                break;
+            }
+
+            generation++;
+            Sleep(1000/FPS);
+            system("cls");
+        }
+        system("pause");
+    }
+
+
+};
 
 int main()
 {
-    GameArea Game = GameArea(25, 25);
-
-    Game.InitializeRandom(25);
-    //Game.InitializePattern("glider");
-
-    unsigned int generation = 1;
-
-    while (true) {
-        Game.UpdateGraph();
-
-        cout << "============================================================================================================" << endl;
-        cout << "   Generation: " << generation << "   |   Alive: " << Game.current_percent << "%   |   Press Ctrl+C to exit" << endl;
-        cout << "============================================================================================================" << endl;
-
-        Game.PrintArea();
-        next_step(Game.count, Game.area, Game.Hight, Game.Width, Game.new_area);
-
-        if (!Game.UpdateFrame()) {
-            cout << "============================================================================================================" << endl;
-            cout << " Game over! Simulation stabilized at generation " << generation << "!" << endl;
-            cout << "============================================================================================================" << endl;
-            break;
-        }
-
-        generation++;
-        Sleep(500);
-        system("cls");
-    }
-    system("pause");
+    Game NewGame = Game(25, 25, 2);
+    NewGame.StartSimulation();
+    return 0;
 }
--- GameOfLife/core.asm
+++ GameOfLife/core.asm
@@ -286,4 +286,444 @@ next_step proc
 		ret
 next_step endp
 
+;==================================================
+;	Tor field simulation
+;==================================================
+next_tor_step proc
+; extern "C" void next_tor_step(int count, bool* area, int hight, int width, bool* new_area);
+; Параметры:
+;	RCX - Размер поля (длина Hight * Width)
+;	RDX - Адрес старого массива
+;	R8 - Количество точек в высоту
+;	R9 - Количество точек в ширину
+; Переменные:
+;	R10 - Счётчик пройденных точек в массиве
+;	R11 - Счётчик живых точек вокруг
+;	R12 - Адрес старого массива из RDX
+;	R13B - Значение конкретной проверяемой точки
+;	R14 - Адрес нового массива
+
+	; Записываем значения регистров Windows в стек
+	push r12
+	push r13
+	push r14
+
+	mov r12, rdx
+	mov r14, qword ptr [rsp + 64]
+	mov r10, 0
+	NextStep:
+		; 1. Выход из проверки массива, точки закончились
+		cmp r10, rcx
+		je ExitLoop
+
+		; 2. Находим значение в данной точке
+		mov r13b, byte ptr [r12 + r10]
+
+		; 3. Очищаем счётчик живых точек
+		mov r11, 0
+
+		; 4. Выполняем деление Количества пройденных точек (R10) на Количество точек в ширину (R9)
+		mov eax, r10d
+		xor edx, edx
+		div r9d
+
+		; 5. Проверяем наличие соседних точек слева (и сверху и снизу слева)
+		cmp edx, 0
+		je CheckLeftPoint
+		; 6. Проверяем наличие точек соседних точек справа
+		dec r9
+		cmp edx, r9d
+		je CheckRightPoint
+		; 7. Проверяем наличие точек соседних точек сверху
+		inc r9
+		cmp eax, 0
+		je CheckTopPoint
+		; 8. Проверяем наличие точек соседних точек снизу
+		dec r8
+		cmp r8, rax
+		je CheckBottomPoint
+		; 9. Иначе, точка находится в центре
+		inc r8
+		; Прибавляем верхнюю точку
+		mov rax, r10
+		sub rax, r9
+		add r11b, byte ptr [r12 + rax]
+
+		; Прибавляем верхнюю левую точку
+		add r11b, byte ptr [r12 + rax - 1]
+
+		; Прибавляем левую точку
+		add r11b, byte ptr [r12 + r10 - 1]
+
+		; Прибавляем верхнюю правую точку
+		add r11b, byte ptr [r12 + rax + 1]
+
+		; Прибавляем правую точку
+		add r11b, byte ptr [r12 + r10 + 1]
+		
+		; Прибавляем нижнюю точку
+		mov rax, r10
+		add rax, r9
+		add r11b, byte ptr [r12 + rax]
+
+		; Прибавляем нижнюю левую точку
+		add r11b, byte ptr [r12 + rax - 1]
+
+		; Прибавляем нижнюю правую точку
+		add r11b, byte ptr [r12 + rax + 1]
+
+		jmp CheckStatusCurPoint
+
+	; Блок подсчёта живых точек вокруг нижней точки
+	CheckBottomPoint:
+		inc r8
+
+		; Прибавляем левую точку
+		add r11b, byte ptr [r12 + r10 - 1]
+
+		; Прибавляем правую точку
+		add r11b, byte ptr [r12 + r10 + 1]
+
+		; Прибавляем верхнюю точку
+		mov rax, r10
+		sub rax, r9
+		add r11b, byte ptr [r12 + rax]
+
+		; Прибавляем верхнюю левую точку
+		add r11b, byte ptr [r12 + rax - 1]
+
+		; Прибавляем верхнюю правую точку
+		add r11b, byte ptr [r12 + rax + 1]
+
+		; Прибавляем нижнюю точку
+		mov rax, r8
+		dec rax
+		imul rax, r9
+		neg rax
+		add rax, r10
+		add r11b, byte ptr [r12 + rax]
+
+		; Прибавляем нижнюю левую точку
+		add r11b, byte ptr [r12 + rax - 1]
+
+		; Прибавляем нижнюю правую точку
+		add r11b, byte ptr [r12 + rax + 1]
+		
+		jmp CheckStatusCurPoint
+
+	; Блок подсчёта живых точек вокруг верхней точки
+	CheckTopPoint:
+		; Прибавляем левую точку
+		add r11b, byte ptr [r12 + r10 - 1]
+
+		; Прибавляем правую точку
+		add r11b, byte ptr [r12 + r10 + 1]
+
+		; Прибавляем нижнюю точку
+		mov rax, r10
+		add rax, r9
+		add r11b, byte ptr [r12 + rax]
+
+		; Прибавляем нижнюю левую точку
+		add r11b, byte ptr [r12 + rax - 1]
+
+		; Прибавляем нижнюю правую точку
+		add r11b, byte ptr [r12 + rax + 1]
+
+		; Прибавляем верхнюю точку
+		mov rax, r8
+		dec rax
+		imul rax, r9
+		add rax, r10
+		add r11b, byte ptr [r12 + rax]
+
+		; Прибавляем верхнюю левую точку
+		add r11b, byte ptr [r12 + rax - 1]
+
+		; Прибавляем верхнюю правую точку
+		add r11b, byte ptr [r12 + rax + 1]
+		
+		jmp CheckStatusCurPoint
+
+	; Блок подсчёта живых точек вокруг правой точки
+	CheckRightPoint:
+		; Проверяем, занимает ли точка верхний правый угол
+		inc r9
+		cmp eax, 0
+		je CheckRightTopPoint
+		; Проверяем, занимает ли точка нижний правый угол
+		dec r8
+		cmp eax, r8d
+		je CheckRightBottomPoint
+		; Иначе, точка занимает середину правой стороны
+		inc r8
+
+		; Прибавляем левую точку
+		add r11b, byte ptr [r12 + r10 - 1]
+
+		; Прибавляем верхнюю точку
+		mov rax, r10
+		sub rax, r9
+		add r11b, byte ptr [r12 + rax]
+
+		; Прибавляем верхнюю левую точку
+		add r11b, byte ptr [r12 + rax - 1]
+
+		; Прибавляем нижнюю точку
+		mov rax, r10
+		add rax, r9
+		add r11b, byte ptr [r12 + rax]
+
+		; Прибавляем нижнюю левую точку
+		add r11b, byte ptr [r12 + rax - 1]
+
+		; Прибавляем нижнюю правую точку
+		mov rax, r10
+		inc rax
+		add r11b, byte ptr [r12 + rax]
+
+		; Прибавляем правую точку
+		sub rax, r9
+		add r11b, byte ptr [r12 + rax]
+
+		; Прибавляем правую точку
+		sub rax, r9
+		add r11b, byte ptr [r12 + rax]
+
+		jmp CheckStatusCurPoint
+
+	; Блок подсчёта живых точек вокруг правой нижней точки
+	CheckRightBottomPoint:
+		inc r8
+		; Прибавляем верхнюю точку
+		mov rax, r10
+		sub rax, r9
+		add r11b, byte ptr [r12 + rax]
+
+		; Прибавляем верхнюю левую точку
+		add r11b, byte ptr [r12 + rax - 1]
+
+		; Прибавляем левую точку
+		add r11b, byte ptr [r12 + r10 - 1]
+
+		; Прибавляем нижнюю точку
+		dec r8
+		mov rax, r8
+		imul rax, r9
+		sub r10, rax
+		add r11b, byte ptr [r12 + r10]
+
+		; Прибавляем нижнюю левую точку
+		add r11b, byte ptr [r12 + r10 - 1]
+		add r10, rax
+		inc r8
+
+		; Прибавляем нижнюю правую точку
+		add r11b, byte ptr [r12]
+
+		; Прибавляем правую точку
+		dec r9
+		mov rax, r10
+		sub rax, r9
+		add r11b, byte ptr [r12 + rax]
+		inc r9
+
+		; Прибавляем верхнюю правую точку
+		sub rax, r9
+		add r11b, byte ptr [r12 + rax]
+		
+		jmp CheckStatusCurPoint
+
+	; Блок подсчёта живых точек вокруг правой верхней точки
+	CheckRightTopPoint:
+		; Прибавляем левую точку
+		add r11b, byte ptr [r12 + r10 - 1]
+
+		; Прибавляем нижнюю точку
+		mov rax, r10
+		add rax, r9
+		add r11b, byte ptr [r12 + rax]
+
+		; Прибавляем нижнюю левую точку
+		add r11b, byte ptr [r12 + rax - 1]
+
+		; Прибавляем правую точку
+		add r11b, byte ptr [r12]
+
+		; Прибавляем нижнюю правую точку
+		add r11b, byte ptr [r12 + r9]
+
+		; Прибавляем верхнюю правую точку
+		dec r8
+		mov rax, r8
+		imul rax, r9
+		inc r8
+		add r11b, byte ptr [r12 + rax]
+
+		; Прибавляем верхнюю точку
+		dec r9
+		add rax, r9
+		inc r9
+		add r11b, byte ptr [r12 + rax]
+
+		; Прибавляем верхнюю левую точку
+		add r11b, byte ptr [r12 + rax - 1]
+		
+		jmp CheckStatusCurPoint
+
+	; Блок подсчёта живых точек вокруг левой точки
+	CheckLeftPoint:
+		; Проверяем, занимает ли точка верхний левый угол
+		cmp eax, 0
+		je CheckLeftTopPoint
+		; Проверяем, занимает ли точка нижний левый угол
+		dec r8
+		cmp eax, r8d
+		je CheckLeftBottomPoint
+		; Иначе, точка занимает середину левой стороны
+		inc r8
+
+		; Прибавляем правую точку
+		add r11b, byte ptr [r12 + r10 + 1]
+
+		; Прибавляем верхнюю точку
+		mov rax, r10
+		sub rax, r9
+		add r11b, byte ptr [r12 + rax]
+
+		; Прибавляем верхнюю правую точку
+		add r11b, byte ptr [r12 + rax + 1]
+
+		; Прибавляем нижнюю точку
+		mov rax, r10
+		add rax, r9
+		add r11b, byte ptr [r12 + rax]
+
+		; Прибавляем нижнюю правую точку
+		add r11b, byte ptr [r12 + rax + 1]
+
+		; Прибавляем верхнюю левую точку
+		mov rax, r10
+		dec rax
+		add r11b, byte ptr [r12 + rax]
+
+		; Прибавляем левую точку
+		add rax, r9
+		add r11b, byte ptr [r12 + rax]
+
+		; Прибавляем нижнюю левую точку
+		add rax, r9
+		add r11b, byte ptr [r12 + rax]
+
+		jmp CheckStatusCurPoint
+
+	; Блок подсчёта живых точек вокруг левой верхней точки
+	CheckLeftTopPoint:
+		; Прибавляем правую точку
+		add r11b, byte ptr [r12 + 1]
+
+		; Прибавляем нижнюю точку
+		add r11b, byte ptr [r12 + r9]
+
+		; Прибавляем нижнюю правую точку
+		add r11b, byte ptr [r12 + r9 + 1]
+
+		; Прибавляем левую точку
+		mov rax, r9
+		dec rax
+		add r11b, byte ptr [r12 + rax]
+
+		; Прибавляем нижнюю левую точку
+		add rax, r9
+		add r11b, byte ptr [r12 + rax]
+
+		; Прибавляем верхнюю точку
+		mov rax, r8
+		dec rax
+		imul rax, r9
+		add r11b, byte ptr [r12 + rax]
+
+		; Прибавляем верхнюю правую точку
+		add r11b, byte ptr [r12 + rax + 1]
+
+		; Прибавляем верхнюю левую точку
+		mov rax, r8
+		imul rax, r9
+		dec rax
+		add r11b, byte ptr [r12 + rax]
+		
+		jmp CheckStatusCurPoint
+
+	; Блок подсчёта живых точек вокруг левой нижней точки
+	CheckLeftBottomPoint:
+		inc r8
+
+		; Прибавляем верхнюю точку
+		mov rax, r10
+		sub rax, r9
+		add r11b, byte ptr [r12 + rax]
+
+		; Прибавляем верхнюю правую точку
+		add r11b, byte ptr [r12 + rax + 1]
+
+		; Прибавляем правую точку
+		add r11b, byte ptr [r12 + r10 + 1]
+
+		; Прибавляем нижнюю точку
+		add r11b, byte ptr [r12]
+
+		; Прибавляем нижнюю правую точку
+		add r11b, byte ptr [r12 + 1]
+
+		; Прибавляем нижнюю левую точку
+		add r11b, byte ptr [r12 + r9 - 1]
+
+		; Прибавляем верхнюю левую точку
+		add r11b, byte ptr [r12 + r10 - 1]
+
+		; Прибавляем левую точку
+		mov rax, r10
+		add rax, r9
+		add r11b, byte ptr [r12 + rax - 1]
+		
+		jmp CheckStatusCurPoint
+	
+	CheckStatusCurPoint:
+		cmp r13, 1
+		je CheckAlive
+		jmp CheckDead
+
+	CheckAlive:
+		cmp r11, 2
+		je KeepAlive
+		cmp r11, 3
+		je KeepAlive
+		
+		mov byte ptr [r14 + r10], 0
+		jmp Increment
+	KeepAlive:
+		mov byte ptr [r14 + r10], 1
+		jmp Increment
+
+	CheckDead:
+		cmp r11, 3
+		jne KeepDead
+		
+		mov byte ptr [r14 + r10], 1
+		jmp Increment
+	KeepDead:
+		mov byte ptr [r14 + r10], 0
+		jmp Increment
+
+	Increment:
+		inc r10
+		jmp NextStep
+
+	ExitLoop:
+		pop r14
+		pop r13
+		pop r12
+		ret
+next_tor_step endp
+
 end
